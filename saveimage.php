5d782845021f473fae878725235e0668e51fd616
vCarbonnn <carbonontagpro@gmail.com>
2016-11-05 22:34:24 +0000

    Update saveimage.php

--- saveimage.php
+++ saveimage.php
@@ -1,11 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');  
-if(isset($GLOBALS["HTTP_RAW_POST_DATA"])) {
-	$rawImage=$GLOBALS['HTTP_RAW_POST_DATA'];
-	$removeHeaders=substr($rawImage,strpos($rawImage, ",")+1);
-	$decode=base64_decode($removeHeaders);
-	$fopen=fopen('myImage.png', 'wb');
-	fwrite($fopen,$decode);
-	fclose($fopen);
-}
+header("Access-Control-Allow-Origin: *");
+$rawImage=$GLOBALS['HTTP_RAW_POST_DATA'];
+$removeHeaders=substr($rawImage,strpos($rawImage, ",")+1);
+$decode=base64_decode($removeHeaders);
+$fopen=fopen('myImage.png', 'wb');
+fwrite($fopen,$decode);
+fclose($fopen);
 ?>
