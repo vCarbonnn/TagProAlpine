3757b7dac6638d69d4948b4ce7449455a34cf625
vCarbonnn <carbonontagpro@gmail.com>
2016-11-05 22:33:11 +0000

    Update saveimage.php

--- saveimage.php
+++ saveimage.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: *');  
 if(isset($GLOBALS["HTTP_RAW_POST_DATA"])) {
 	$rawImage=$GLOBALS['HTTP_RAW_POST_DATA'];
 	$removeHeaders=substr($rawImage,strpos($rawImage, ",")+1);
