4bb3684ba82da5bbe822cea89869199ba201ec15
vCarbonnn <carbonontagpro@gmail.com>
2016-11-06 21:50:33 +0000

    Update saveimage.php

--- saveimage.php
+++ saveimage.php
@@ -3,7 +3,12 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
+echo "Alpine Test";
+
 $rawImage=$GLOBALS['HTTP_RAW_POST_DATA'];
+
+echo $rawImage;
+
 $removeHeaders=substr($rawImage,strpos($rawImage, ",")+1);
 $decode=base64_decode($removeHeaders);
 $fopen=fopen('myImage.png', 'wb');
